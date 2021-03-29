a3192ec423b8956041dea4b9c17e082cd901bfc0
AlexandrNistratov <koss.1991@yandex.ru>
2021-03-30 00:14:27 +0300

    edit utils.js

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -1,1 +1,2 @@
-export const base_url = `${window.location.protocol}${process.env.REACT_APP_API_URL || '//localhost:3001'}`;
+// export const base_url = `${window.location.protocol}${process.env.REACT_APP_API_URL || '//localhost:3001'}`;
+export const base_url = 'api.domainname.students.nomoredomains.club';
