50bf328c2ed11aa47b5de33b7a0f929b138e8bd8
AlexandrNistratov <koss.1991@yandex.ru>
2021-03-30 19:29:44 +0300

    delete .env

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -1,2 +1,1 @@
-// export const base_url = `${window.location.protocol}${process.env.REACT_APP_API_URL || '//localhost:3001'}`;
-export const base_url = 'api.domainname.students.nomoredomains.club';
+export const base_url = `${window.location.protocol}${process.env.REACT_APP_API_URL || '//localhost:3001'}`;
